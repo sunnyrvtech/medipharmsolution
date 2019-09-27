8dee5adf472015d978b702633c6c9a7984fb6821
diksha <diksha@rvtechnologies.co.in>
2019-09-27 10:38:38 +0530

    27sept

---
+++ backend/www/precache-manifest.09f31fdc7d53f15374dc0d08aa79ce2a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2aff1f5393bf0ee2b403",
+    "url": "/static/js/main.2aff1f53.chunk.js"
+  },
+  {
+    "revision": "8ec0b3a14358ff0daa91",
+    "url": "/static/js/1.8ec0b3a1.chunk.js"
+  },
+  {
+    "revision": "2aff1f5393bf0ee2b403",
+    "url": "/static/css/main.16ff746e.chunk.css"
+  },
+  {
+    "revision": "8ec0b3a14358ff0daa91",
+    "url": "/static/css/1.2f167c7e.chunk.css"
+  },
+  {
+    "revision": "6dd34efe9c577b4e89c2828f96b93357",
+    "url": "/index.html"
+  }
+];
